85fe2dd31007d89db5cd124334cb8505b3633f1b
Dark Chyper <services-gitlab.com@dark-chyper.com>
2020-06-14 20:02:07 +0200

    base d'appel à l'API -> bad request

---
+++ src/Exception/CallAPIException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Exception;
+
+
+class CallAPIException extends \RuntimeException
+{
+
+}
--- src/Service/CallRequestService.php
+++ src/Service/CallRequestService.php
@@ -5,8 +5,16 @@ namespace App\Service;
 
 
 use App\Entity\CallRequest;
+use App\Exception\CallAPIException;
 use Doctrine\ORM\EntityManagerInterface;
+use http\Exception\RuntimeException;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class CallRequestService
 {
@@ -38,9 +46,35 @@ class CallRequestService
      * @return CallRequest
      */
     public function validateCallRequest(CallRequest $callRequest){
+        //$this->validateByAPI($callRequest->getCountry(), $callRequest->getPhoneNumber());
         $callRequest->setNational($callRequest->getPhoneNumber());
         $callRequest->setInternational($callRequest->getPhoneNumber());
         return $callRequest;
+    }
+
+    public function validateByAPI(String $country, String $phoneNumber){
+        $client = HttpClient::createForBaseUri('http://163.172.67.144:8042/',[
+           'auth_basic' => 'api:azpihviyazfb'
+        ]);
+
+        try {
+            $response = $client->request('POST', 'http://163.172.67.144:8042/api/v1/validate', [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => ['phoneNumber' => $phoneNumber, "countryCode" => $country]
+            ]);
+            var_dump($response->toArray());
+
+        } catch (TransportExceptionInterface |
+                ClientExceptionInterface |
+                DecodingExceptionInterface |
+                RedirectionExceptionInterface |
+                ServerExceptionInterface $e) {
+
+            throw new CallAPIException($e);
+        }
+
     }
 
     /**
